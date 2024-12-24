962b7a3152f41df97c142d50365a148e4031daa7
Divyansh Tripathi <85444838+divyansh-tripathi7@users.noreply.github.com>
2024-12-24 11:41:07 +0530

    basic streamlit app

---
+++ st.py
@@ -0,0 +1,229 @@
+import streamlit as st
+from crewai import Agent, Task, Crew, Process
+import os
+from crewai_tools import ScrapeWebsiteTool, SerperDevTool,   FileReadTool, MDXSearchTool,
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from docx import Document
+from io import BytesIO
+import base64
+
+load_dotenv()
+
+# LLM object and API Key
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
+
+search_tool = SerperDevTool()
+scrape_tool = ScrapeWebsiteTool()
+read_resume = FileReadTool(file_path='/content/Divyansh_Intern1.pdf')
+semantic_search_resume = MDXSearchTool(mdx='/content/Divyansh_Intern1.md')
+
+
+def generate_docx(result):
+    doc = Document()
+    doc.add_heading('Modified resume and interview recommendations', 0)
+    doc.add_paragraph(result)
+    bio = BytesIO()
+    doc.save(bio)
+    bio.seek(0)
+    return bio
+
+def get_download_link(bio, filename):
+    b64 = base64.b64encode(bio.read()).decode()
+    return f'<a href="data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,{b64}" download="{filename}">Download resume and tips</a>'
+
+st.set_page_config(
+    layout="wide"
+)
+
+# Title
+st.title("Resume Enhancer")
+
+
+# Text Inputs
+resume = st.file_uploader('Choose your .pdf file', type="pdf")
+job_posting_url = st.text_area('URL of job to apply for')
+personal_writeup = st.text_area('Your writeup', 'e.g., work exp interests and tech stack ')
+
+# Initialize Tools
+search_tool = SerperDevTool()
+scrape_tool = ScrapeWebsiteTool()
+
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo-16k",
+    temperature=0.1,
+    max_tokens=8000
+)
+
+# Define Agents
+ # Agent 1: Researcher
+researcher = Agent(
+    role="Tech Job Researcher",
+    goal="Make sure to do amazing analysis on "
+         "job posting to help job applicants",
+    tools = [scrape_tool, search_tool],
+    verbose=True,
+    backstory=(
+        "As a Job Researcher, your prowess in "
+        "navigating and extracting critical "
+        "information from job postings is unmatched."
+        "Your skills help pinpoint the necessary "
+        "qualifications and skills sought "
+        "by employers, forming the foundation for "
+        "effective application tailoring."
+    )
+)
+
+ # Agent 2: Profiler
+profiler = Agent(
+    role="Personal Profiler for Engineers",
+    goal="Do increditble research on job applicants "
+         "to help them stand out in the job market",
+    tools = [scrape_tool, search_tool,
+             read_resume, semantic_search_resume],
+    verbose=True,
+    backstory=(
+        "Equipped with analytical prowess, you dissect "
+        "and synthesize information "
+        "from diverse sources to craft comprehensive "
+        "personal and professional profiles, laying the "
+        "groundwork for personalized resume enhancements."
+    )
+)
+
+ # Agent 3: Resume Strategist
+resume_strategist = Agent(
+    role="Resume Strategist for Engineers",
+    goal="Find all the best ways to make a "
+         "resume stand out in the job market.",
+    tools = [scrape_tool, search_tool,
+             read_resume, semantic_search_resume],
+    verbose=True,
+    backstory=(
+        "With a strategic mind and an eye for detail, you "
+        "excel at refining resumes to highlight the most "
+        "relevant skills and experiences, ensuring they "
+        "resonate perfectly with the job's requirements."
+    )
+)
+
+
+ # Agent 4: Interview Preparer
+interview_preparer = Agent(
+    role="Engineering Interview Preparer",
+    goal="Create interview questions and talking points "
+         "based on the resume and job requirements",
+    tools = [scrape_tool, search_tool,
+             read_resume, semantic_search_resume],
+    verbose=True,
+    backstory=(
+        "Your role is crucial in anticipating the dynamics of "
+        "interviews. With your ability to formulate key questions "
+        "and talking points, you prepare candidates for success, "
+        "ensuring they can confidently address all aspects of the "
+        "job they are applying for."
+    )
+)
+
+
+# Define Tasks
+ # Task for Researcher Agent: Extract Job Requirements
+research_task = Task(
+    description=(
+        "Analyze the job posting URL provided ({job_posting_url}) "
+        "to extract key skills, experiences, and qualifications "
+        "required. Use the tools to gather content and identify "
+        "and categorize the requirements."
+    ),
+    expected_output=(
+        "A structured list of job requirements, including necessary "
+        "skills, qualifications, and experiences."
+    ),
+    agent=researcher,
+    async_execution=True
+)
+
+
+ # Task for Profiler Agent: Compile Comprehensive Profile
+profile_task = Task(
+    description=(
+        "Compile a detailed personal and professional profile "
+        "using the GitHub ({github_url}) URLs, and personal write-up "
+        "({personal_writeup}). Utilize tools to extract and "
+        "synthesize information from these sources."
+    ),
+    expected_output=(
+        "A comprehensive profile document that includes skills, "
+        "project experiences, contributions, interests, and "
+        "communication style."
+    ),
+    agent=profiler,
+    async_execution=True
+)
+
+ # Task for Resume Strategist Agent: Align Resume with Job Requirements
+resume_strategy_task = Task(
+    description=(
+        "Using the profile and job requirements obtained from "
+        "previous tasks, tailor the resume to highlight the most "
+        "relevant areas. Employ tools to adjust and enhance the "
+        "resume content. Make sure this is the best resume even but "
+        "don't make up any information. Update every section, "
+        "inlcuding the initial summary, work experience, skills, "
+        "and education. All to better reflrect the candidates "
+        "abilities and how it matches the job posting."
+    ),
+    expected_output=(
+        "An updated resume that effectively highlights the candidate's "
+        "qualifications and experiences relevant to the job."
+    ),
+    output_file="tailored_resume.md",
+    context=[research_task, profile_task],
+    agent=resume_strategist
+)
+
+ # Task for Interview Preparer Agent: Develop Interview Materials
+interview_preparation_task = Task(
+    description=(
+        "Create a set of potential interview questions and talking "
+        "points based on the tailored resume and job requirements. "
+        "Utilize tools to generate relevant questions and discussion "
+        "points. Make sure to use these question and talking points to "
+        "help the candiadte highlight the main points of the resume "
+        "and how it matches the job posting."
+    ),
+    expected_output=(
+        "A document containing key questions and talking points "
+        "that the candidate should prepare for the initial interview."
+    ),
+    output_file="interview_materials.md",
+    context=[research_task, profile_task, resume_strategy_task],
+    agent=interview_preparer
+)
+
+# Create Crew
+job_application_crew = Crew(
+    agents=[researcher,
+            profiler,
+            resume_strategist,
+            interview_preparer],
+
+    tasks=[research_task,
+           profile_task,
+           resume_strategy_task,
+           interview_preparation_task],
+
+    verbose=True
+)
+
+# Execution
+if st.button("customise resume and provide interview prep tips"):
+    with st.spinner('Generating results...'):
+        result = crew.kickoff(inputs={"job_posting_url": job_posting_url, "github_url": github_url, "personal_writeup":personal_writeup })
+        st.write(result)
+        docx_file = generate_docx(result)
+
+        download_link = get_download_link(docx_file, "Enhanced resume and tips.docx")
+        
+        st.markdown(download_link, unsafe_allow_html=True)
